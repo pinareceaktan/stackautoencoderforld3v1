b0e09735e72ec6ce8d73c4ca444adf6de21674ac
pinarece_fera <pinareceaktan@gmail.com>
2016-07-21 16:17:25 +0300

    Rotate points m

---
+++ rotate_points.m
@@ -0,0 +1,14 @@
+function [rotated_landmarks] = rotate_points(points,theta)
+theta = -theta*(pi/180);  
+% define a 60 degree counter-clockwise rotation matrix
+R = [cos(theta) -sin(theta); sin(theta) cos(theta)];
+center = repmat([0; 0], 1, length(points));
+v = double(points') - center;
+
+% do the rotation...
+so = R*v;           % apply the rotation about the origin
+vo = so + center;
+% this can be done in one line as:
+% vo = R*(v - center) + center
+% pick out the vectors of rotated x- and y-data
+rotated_landmarks = [vo(1,:);vo(2,:)]';
