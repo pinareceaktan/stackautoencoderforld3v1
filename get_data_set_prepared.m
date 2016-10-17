90bbb69a2a5af97d88e736e2195e0a0d232439ad
pinareceaktan <pinareceaktan@gmail.com>
2016-10-17 12:33:20 +0300

    Function has modified to partition data according to the pose classes.

--- get_data_set_prepared.m
+++ get_data_set_prepared.m
@@ -1,17 +1,28 @@
-function [data_mat,landmarks_mat,labels_mat] = get_data_set_prepared(data_to_be_parsed)
+function [data_mat,landmarks_mat,labels_mat] = get_data_set_prepared(data_to_be_parsed,pose_partition)
 imsize       = 50*50;
 landmarksize = 68*2;
 samplesize   = size(data_to_be_parsed,2);
 data_mat = zeros(imsize,samplesize);
 landmarks_mat  = zeros(landmarksize,samplesize);
 labels_mat     = zeros(1,samplesize);
+if ~pose_partition
 for i = 1:size(data_to_be_parsed,2)
     try
     data_mat(:,i) = data_to_be_parsed(i).face;
     landmarks_mat(:,i)  = data_to_be_parsed(i).groundtruth;
+    labels_mat(:,i) = data_to_be_parsed(i).pose;
 %     labels_mat(:,i)     = data(i).pose;
     catch ME
         disp(i);
         disp(ME.identifier);
     end
+end
+else
+    prompt  = 'Which pose do you want to fetch ? ';
+    pose    = input(prompt);
+%     [data_to_be_parsed.pose].'
+    indices = find([data_to_be_parsed.pose] == pose(1,1));
+    data_mat = double([data_to_be_parsed(indices).face]);
+    landmarks_mat = [data_to_be_parsed(indices).groundtruth];
+    labels_mat = [data_to_be_parsed(indices).pose];
 end
