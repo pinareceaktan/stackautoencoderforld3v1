41936970622343017d06925903ebfb8a6d4ea4b7
pinareceaktan <pinareceaktan@gmail.com>
2016-10-28 21:25:00 +0300

    Architecture changing : Throw out the perceptron hidden layers

--- initializePerceptronParams.m
+++ initializePerceptronParams.m
@@ -1,11 +1,18 @@
-function theta = initializePerceptronParams(outputSize,hiddenSize,inputSize)
+function theta = initializePerceptronParams(outputSize,inputSize)
 %% Initialize parameters randomly based on layer sizes.
-r  = sqrt(6) / sqrt(outputSize+hiddenSize+inputSize+1);   % we'll choose weights uniformly from the interval [-r, r]
-W1 = rand(hiddenSize, inputSize) * 2 * r - r; % hidden layer * input layer sized
-W2 = rand(outputSize, hiddenSize) * 2 * r - r; % final layer * hidden layer sized
-b1 = zeros(hiddenSize, 1); % input layer * 1 sized
-b2 = zeros(outputSize, 1); % hidden layer *1 sized
+% r  = sqrt(6) / sqrt(outputSize+hiddenSize+inputSize+1);   % we'll choose weights uniformly from the interval [-r, r]
+% W1 = rand(hiddenSize, inputSize) * 2 * r - r; % hidden layer * input layer sized
+% W2 = rand(outputSize, hiddenSize) * 2 * r - r; % final layer * hidden layer sized
+% b1 = zeros(hiddenSize, 1); % input layer * 1 sized
+% b2 = zeros(outputSize, 1); % hidden layer *1 sized
 
-theta = [W1(:) ; W2(:) ; b1(:) ; b2(:)];
+% theta = [W1(:) ; W2(:) ; b1(:) ; b2(:)];
+
+
+r  = sqrt(6) / sqrt(outputSize+inputSize+1);   % we'll choose weights uniformly from the interval [-r, r]
+W1 = rand(outputSize, inputSize) * 2 * r - r; % hidden layer * input layer sized
+b1 = zeros(outputSize, 1); % hidden layer *1 sized
+
+theta = [W1(:) ; b1(:) ];
 
 end
