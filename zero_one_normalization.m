556ce2e68504d9be789f7358a172954436246314
pinarece_fera <pinareceaktan@gmail.com>
2016-11-04 15:26:35 +0300

    Update

--- zero_one_normalization.m
+++ zero_one_normalization.m
@@ -1,4 +1,6 @@
 function [normalizedim] = zero_one_normalization(pic)
+%% This comment is for github
     normalizedim = (pic(:) - min(pic(:))) / ( max(pic(:)) - min(pic(:)) );
     normalizedim = reshape(normalizedim, size(pic, 1), size(pic,2));
+
 end
