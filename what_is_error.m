d9a410c5c2e74d190be24ee03d74a8ad8ced92f6
pinarece_fera <pinareceaktan@gmail.com>
2016-11-24 13:23:51 +0300

    What is error func. modified to work in accordance with test  data.

--- what_is_error.m
+++ what_is_error.m
@@ -1,24 +1,10 @@
 % error
-% load('landmark_prd.mat') ;
-% load('testdatadenormalized.mat');
-% 
-% for i = 1: size(testdatadenormalized,2)
-%     gt(:,:,i) = [reshape(testdatadenormalized(i).groundtruth,68,2)];
-%     predictions(:,:,i) = [reshape(landmark_prd(:,i),68,2)];
-% end
-% 
-%  [ error_per_image ] = compute_error( gt, predictions);
-%  
-%  err_mat(:,1) = numel(find(error_per_image(:)>0.1));
-%  err_mat(:,2) = numel(find(error_per_image(:)>0.15));
-%  err_mat(:,3) = numel(find(error_per_image(:)>0.2));
+load('landmark_prd.mat') ;
+load('testdatadenormalized.mat');
 
-load('landmark_prd_on_train.mat') ;
-load('datadenormalized.mat');
-
-for i = 1: size(datadenormalized,2)
-    gt(:,:,i) = [reshape(datadenormalized(i).groundtruth,68,2)];
-    predictions(:,:,i) = [reshape(landmark_prd_on_train(:,i),68,2)];
+for i = 1: size(testdatadenormalized,2)
+    gt(:,:,i) = [reshape(testdatadenormalized(i).groundtruth,68,2)];
+    predictions(:,:,i) = [reshape(landmark_prd(:,i),68,2)];
 end
 
  [ error_per_image ] = compute_error( gt, predictions);
@@ -26,5 +12,19 @@ end
  err_mat(:,1) = numel(find(error_per_image(:)>0.1));
  err_mat(:,2) = numel(find(error_per_image(:)>0.15));
  err_mat(:,3) = numel(find(error_per_image(:)>0.2));
- x = err_mat*1/size(datadenormalized,2);
+
+% load('landmark_prd_on_train.mat') ;
+% load('datadenormalized.mat');
+% 
+% for i = 1: size(datadenormalized,2)
+%     gt(:,:,i) = [reshape(datadenormalized(i).groundtruth,68,2)];
+%     predictions(:,:,i) = [reshape(landmark_prd_on_train(:,i),68,2)];
+% end
+% 
+%  [ error_per_image ] = compute_error( gt, predictions);
+%  
+%  err_mat(:,1) = numel(find(error_per_image(:)>0.1));
+%  err_mat(:,2) = numel(find(error_per_image(:)>0.15));
+%  err_mat(:,3) = numel(find(error_per_image(:)>0.2));
+%  x = err_mat*1/size(datadenormalized,2);
 
